6762501e5baa5b778b3036cab1c2fc47042b7147
ReemAl-Azmi <rrmm7747@gmail.com>
2024-10-16 01:50:38 +0300

    add lookForFacility fun

--- data/facility.js
+++ data/facility.js
@@ -1,33 +1,33 @@
 export const facilities = [{
-  id: 'cafe',
+  id: "cafe",
   image:"../img/cafe-coffee-cup-svgrepo-com.svg",
   title: 'مقهى'
 }, {
-  id: 'restaurant',
+  id: "restaurant",
   image: "../img/restaurant-svgrepo-com.svg",
   title: 'مطعم'
 }, {
-  id: 'store',
+  id: "grocery",
   image:"../img/shop-cart-svgrepo-com.svg",
   title: 'بقالة'
 }, {
-  id: 'mosque',
+  id: "mosque",
   image:"../img/pray-day-svgrepo-com.svg",
   title: 'مصلى'
 }, {
-  id: 'stadium',
+  id: "stadium",
   image:"../img/basketball-svgrepo-com.svg",
   title: 'ملعب كرة السلة'
 }, {
-  id: 'stadium',
+  id: "stadium",
   image:"../img/football-svgrepo-com.svg",
   title: 'ملعب كرة القدم'
 }, {
-  id: 'stadium',
+  id: "stadium",
   image:"../img/gym-svgrepo-com.svg",
   title: ' الصالة الرياضية'
 }, {
-  id: 'hospital',
+  id: "hospital",
   image:"../img/hospital-svgrepo-com.svg",
   title: ' المستشفى الجامعي '
 }]
@@ -37,7 +37,7 @@ function renderFacility() {
 
   facilities.forEach((facility) => {
     facilityHTML += `
-      <button class="facility-button">
+      <button class="facility-button js-look-for-facility" data-facility-id="${facility.id}">
           <img class="facility-icon" src=${facility.image}>
           <div class="icon-title">
               ${facility.title}
--- script/map.js
+++ script/map.js
@@ -1,5 +1,5 @@
 import { uniFacility } from '../data/AJData.js';
-
+import { facilities } from '../data/facility.js';
 
 let map = L.map('map').setView([27.563073576201173, 41.700262995401836],16.4);
 
@@ -8,4 +8,43 @@ L.tileLayer('https://api.maptiler.com/maps/satellite/{z}/{x}/{y}.jpg?key=dHw0UMI
   attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
 }).addTo(map);
 
-L.geoJSON(uniFacility).addTo(map);
+function LookForFacility() {
+  document.querySelectorAll('.js-look-for-facility')
+    .forEach((button) => {
+      button.addEventListener('click', function() {
+        const facilityId = button.getAttribute('data-facility-id'); // الحصول على id المرفق من زر الضغط
+
+        // إزالة جميع الطبقات السابقة من الخريطة
+        map.eachLayer(layer => {
+          if (layer instanceof L.GeoJSON) {
+            map.removeLayer(layer);
+          }
+        });
+
+        // فلترة الميزات بناءً على id المختار
+        const filteredFeatures = uniFacility.features.filter(feature => {
+          return feature.properties.facility === facilityId;
+        });
+
+        if (filteredFeatures.length > 0) {
+          // إضافة GeoJSON المفلتر إلى الخريطة
+          L.geoJSON({
+            type: 'FeatureCollection',
+            features: filteredFeatures
+          } /*
+              add popup
+          , {
+            onEachFeature: function(feature, layer) {
+              layer.bindPopup(feature.properties.name || "Unnamed Facility");
+            }
+          }*/ ).addTo(map);
+        }
+      });
+    });
+}
+
+// استدعاء الوظيفة
+LookForFacility();
+
+
+
