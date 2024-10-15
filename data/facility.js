796902008a655a6a319f3816ceb4d322344cd897
ReemAl-Azmi <rrmm7747@gmail.com>
2024-10-16 00:08:43 +0300

    Add facility data

--- data/facility.js
+++ data/facility.js
@@ -1,5 +1,33 @@
 export const facility = [{
   id: 'cafe',
-  Image:"./img/cafe-coffee-cup-svgrepo-com.svg",
-  
+  image:"./img/cafe-coffee-cup-svgrepo-com.svg",
+  title: 'مقهى'
+}, {
+  id: 'restaurant',
+  image: "./img/restaurant-svgrepo-com.svg",
+  title: 'مطعم'
+}, {
+  id: 'store',
+  image:"./img/shop-cart-svgrepo-com.svg",
+  title: 'بقالة'
+}, {
+  id: 'mosque',
+  image:"./img/pray-day-svgrepo-com.svg",
+  title: 'مصلى'
+}, {
+  id: 'stadium',
+  image:"./img/basketball-svgrepo-com.svg",
+  title: 'ملعب كرة السلة'
+}, {
+  id: 'stadium',
+  image:"./img/football-svgrepo-com.svg",
+  title: 'ملعب كرة القدم'
+}, {
+  id: 'stadium',
+  image:"./img/gym-svgrepo-com.svg",
+  title: ' الصالة الرياضية'
+}, {
+  id: 'hospital',
+  image:"./img/hospital-svgrepo-com.svg",
+  title: ' المستشفى الجامعي '
 }]
